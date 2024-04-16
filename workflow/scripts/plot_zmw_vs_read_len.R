5794c8388c10eba3a01d926eb8293e6f654bafe0
Ethan Thomas Holleman etholleman@ucdavis.edu <etholleman@ucdavis.edu>
2024-04-16 11:31:13 -0700

    update script plotting

--- workflow/scripts/plot_zmw_vs_read_len.R
+++ workflow/scripts/plot_zmw_vs_read_len.R
@@ -7,8 +7,14 @@ input.sam <- snakemake@input[[1]]
 df <- read.table(input.sam, sep = "\t", header = TRUE)
 
 plt <- ggplot(df, aes(y=read_length, x=zmw, color=sample)) + 
-        geom_point()
+        geom_point(size=0.01) + 
+        facet_wrap(~sample, ncol=1) + 
+        scale_color_brewer(palette='Dark2') + 
+        theme_pubr() +
+        theme(legend.position='None') +
+        ylim(0, 3000) +
+        labs(x='ZMW number', y='Read length')
 
 
-ggsave(snakemake@output[[1]], plt)
+ggsave(snakemake@output[[1]], plt, width = 20, height = 8, dpi = 300)
 
